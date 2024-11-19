ffd61a80b496314d152cb49663266c42e1b60cec
JSoi <jsoiworks@gmail.com>
2024-11-19 12:46:46 +0900

    Effective Java Item02

---
+++ src/main/java/effective_java/chapter_1/item_2/Pizza.java
@@ -0,0 +1,24 @@
+package effective_java.chapter_1.item_2;
+
+import java.util.EnumSet;
+import java.util.Objects;
+
+public class Pizza {
+    public enum Topping {
+        HAM, MUSHROOM, ONION, PEPPER, SAUSAGE
+    }
+    final EnumSet<Topping> toppings;
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+        abstract Pizza build();
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
+}

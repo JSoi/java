089f3cc0a0d2bbebe11dd06d80b0dcacbb6fa017
JSoi <jsoiworks@gmail.com>
2025-03-07 16:15:20 +0900

    demo: chain-of-responsibility example

---
+++ src/main/java/design_patterns/behavioral/chain_of_responsibility/NoOpHandler.java
@@ -0,0 +1,13 @@
+package design_patterns.behavioral.chain_of_responsibility;
+
+public class NoOpHandler implements Handler {
+    @Override
+    public boolean canHandle(Request request) {
+        return true; // 어떤 요청도 거부하지 않음
+    }
+
+    @Override
+    public void process(Request request) {
+        // 아무것도 안 함
+    }
+}
